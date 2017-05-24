21c848859089391cc1334ebc4a829439b8b86963
Viktoria Maksimova <viktoria.maksimova@intel.com>
2017-05-24 11:58:51 +0300

    can't build

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,7 @@
 package sample;
 
 public class Controller {
+    public void onClick(){
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Hello World");
+
+        primaryStage.setIconified(false);
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.getScene().setRoot(root);
         primaryStage.show();
     }
 
