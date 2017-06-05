55452fece9c9ba97338428792c0c1c15aef80e71
Igor Smirnov <smirnov_ig@mail.ru>
2017-06-06 00:12:54 +0300

    alpha version gallery - v.0.2
    gallery works based on the library JFoenix.

--- src/sample/ScreenshotController.java
+++ src/sample/ScreenshotController.java
@@ -1,41 +1,30 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXMasonryPane;
 import com.jfoenix.controls.JFXToggleButton;
-//import com.sun.javafx.geom.Rectangle;
-//import com.sun.javafx.tk.Toolkit;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.geometry.*;
-import javafx.geometry.Insets;
-import javafx.scene.canvas.*;
-import javafx.scene.control.ScrollBar;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-//import com.sun.glass.ui.Robot;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
-import javax.annotation.Resource;
 import javax.imageio.ImageIO;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javafx.scene.canvas.Canvas;
+//import com.sun.javafx.geom.Rectangle;
+//import com.sun.javafx.tk.Toolkit;
+//import com.sun.glass.ui.Robot;
 
 public class ScreenshotController {
 
@@ -124,18 +113,9 @@ public class ScreenshotController {
         //=======Catalogue===========================
         //===========================================
 
-        contentGalery();
-
-        scrollY.setMin(0);
-//        scrollY.setMax(100);
-        scrollY.setValue(0);
-
-        tilePane.setPadding(new Insets(10, 10, 10, 10));
-        tilePane.setVgap(15);
-        tilePane.setHgap(20);
-        tilePane.setStyle("-fx-background-color: 000000;");
-        tilePane.setAlignment(Pos.TOP_CENTER);
-
+        ArrayList<Node> children = new ArrayList<>();
+        contentGalery(children);
+        masonryPane.getChildren().addAll(children);
     }
 
     static private int counter = 0;
@@ -187,35 +167,32 @@ public class ScreenshotController {
     //===========================================
 
     @FXML
-    private ScrollBar scrollY;
-    @FXML
-    private TilePane tilePane;
-    @FXML
-    private ImageView imageView00, imageView01, imageView02, imageView03, imageView10, imageView11, imageView12,
-            imageView13, imageView20, imageView21, imageView22, imageView23;
-    @FXML
-    private void scrollMenu(){
-        scrollY.setMax(primaryStage.getHeight()/0.7);
-//        scrollY.setMax();
-        System.out.println(primaryStage.getHeight());
-        scrollY.valueProperty().addListener(event->{
-            tilePane.setTranslateY(-scrollY.getValue());
-        });
-    }
+    JFXMasonryPane masonryPane;
 
-    private void contentGalery(){
-        Image image = new Image("http://333v.ru/uploads/0a/0aa6cf3843b1cffc6c570812b8b304aa.jpg");
-        imageView00.setImage(image);
-        imageView01.setImage(image);
-        imageView02.setImage(image);
-        imageView03.setImage(image);
-        imageView10.setImage(image);
-        imageView11.setImage(image);
-        imageView12.setImage(image);
-        imageView13.setImage(image);
-        imageView20.setImage(image);
-        imageView21.setImage(image);
-        imageView22.setImage(image);
-        imageView23.setImage(image);
+    private void contentGalery(ArrayList children){
+
+//        Image image = new Image("http://333v.ru/uploads/0a/0aa6cf3843b1cffc6c570812b8b304aa.jpg");
+//        Image image2 = new Image("http://333v.ru/uploads/0a/0aa6cf3843b1cffc6c570812b8b304aa.jpg");
+//        ImageView testImageView1 = new ImageView();
+//        ImageView testImageView2 = new ImageView();
+//        testImageView1.setImage(image);
+//        testImageView1.setFitHeight(200);
+//        testImageView1.setFitWidth(200);
+//        testImageView2.setImage(image2);
+//        testImageView2.setFitHeight(200);
+//        testImageView2.setFitWidth(200);
+
+        Random r = new Random();
+        for (int i = 0; i < 30; i++) {
+            javafx.scene.control.Label label = new javafx.scene.control.Label();
+            label.setPrefSize(200, 200);
+            javafx.scene.control.Label label2 = new javafx.scene.control.Label();
+            label2.setPrefSize(200, 200);
+//            label.setGraphic(testImageView1);
+//            label2.setGraphic(testImageView2);
+            label.setStyle("-fx-background-color:rgb(" + r.nextInt(200) + ","+ r.nextInt(200) + ","+ r.nextInt(200) + ");");
+            children.add(label);
+//        children.add(label2);
+        }
     }
 }
